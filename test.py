087ca98939befcb84688b77a7bcdf8edcbbaafb0
Cason Clagg <casonclagg@gmail.com>
2021-10-18 17:01:21 -0500

    a bit of cleanup

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
 from g2p_en import G2p
 g2p = G2p()
-import inflect
-_inflect = inflect.engine()
-print(_inflect.number_to_words(7456.151354, andword=''))
 
 with open("./test.txt") as file:
     lines = file.readlines()
     lines = [line.rstrip() for line in lines]
     for line in lines:
-        print(f"{line}\t\t{g2p(line)}")
+        print(f"{line.ljust(50)}{g2p(line)}")
